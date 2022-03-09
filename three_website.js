1538b69c78d055eca8117708fb029acaea9ed40e
rutuja <rutujaharidas1996@gmail.com>
2022-03-09 09:01:52 -0800

    Installed threejs package from npm

--- three_website.js
+++ three_website.js
@@ -1,7 +1,7 @@
 import * as THREE from './node_modules/three/src/Three.js';
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls'
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 var mouse = { x: 0, y: 0 }
 const scene = new THREE.Scene()
